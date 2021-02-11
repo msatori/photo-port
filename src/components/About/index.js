a6c7d4fbefe7682ff8d60eea433a077a3c2fcd40
Maya Santiago <msatori.code@gmail.com>
2021-02-10 23:44:27 -0600

    rendered about section

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+//import modules
+import React from 'react';
+
+//import assets
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+
+//create function to return About section
+function About() {
+    return (
+        <section className="my-5">
+            <h1 id="about">Who am I?</h1>
+            <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+        </section>
+    );
+}
+
+export default About;
